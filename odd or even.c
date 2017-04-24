eeba4bd253ec95b32e3945764700daabbb74e3c3
akilayuva <akilapadma97@gmail.com>
2017-04-24 13:12:45 +0530

    Create odd or even.c

---
+++ odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a=10,b;
+b=a%2;
+if(b==0)
+{
+printf("given number is even");
+}
+else
+{
+printf("given number is odd");
+}
+getch();
+}
