55c180fbe3270c46d69782a37d98f66dc5bb4d42
akilayuva <akilapadma97@gmail.com>
2017-09-15 16:09:23 +0530

    Create Qpresspro.c

---
+++ Qpresspro.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char ch;
+clrscr();
+printf("Enter the character");
+scanf("%c",&ch);
+while(ch>='a' && ch<='z')
+{
+  if(ch==q && ch==Q)
+  {
+  return 0;
+  }
+ }
+ getch();
+ return 0;
+ }
